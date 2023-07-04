5a4c4747d30b209e8df35cbd79a9290d27c4c229
ByeongNam <41105616+ByeongNam@users.noreply.github.com>
2023-07-04 16:18:21 +0900

    added test for keyboard IO

---
+++ src/main/java/chess/Main.java
@@ -0,0 +1,13 @@
+package chess;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            String str = in.next(); // 입력 문자열 저장
+            System.out.println(str);
+        }
+    }
+}
