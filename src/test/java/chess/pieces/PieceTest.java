aca67bddbae83450afffbb18c866ddf426cb7add
jst0951 <jst0951@gmail.com>
2023-07-05 17:38:18 +0900

    added target test create_piece(), now implementation needed

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -2,6 +2,7 @@ package chess.pieces;
 
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
+import chess.pieces.Piece.Type;
 
 public class PieceTest {
     @Test
@@ -9,5 +10,28 @@ public class PieceTest {
     public void getRepresentationPerPiece() throws Exception {
         assertThat(Piece.Type.PAWN.getWhiteRepresentation()).isEqualTo('p');
         assertThat(Piece.Type.PAWN.getBlackRepresentation()).isEqualTo('P');
+    }
+
+    @Test
+    @DisplayName("색과 기물에 따라 다르게 생성되어야 한다.")
+    public void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Type.PAWN);
+        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Type.KNIGHT);
+        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Type.ROOK);
+        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Type.BISHOP);
+        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Type.QUEEN);
+        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Type.KING);
+
+        Piece blank = Piece.createBlank();
+        assertThat(blank.isWhite()).isFalse();
+        assertThat(blank.isBlack()).isFalse();
+        assertThat(blank.getType()).isEqualTo(Type.NO_PIECE);
+    }
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Type type) {
+        assertThat(whitePiece.isWhite()).isTrue();
+        assertThat(whitePiece.getType()).isEqualTo(type);
+
+        assertThat(blackPiece.isBlack()).isTrue();
+        assertThat(blackPiece.getType()).isEqualTo(type);
     }
 }
