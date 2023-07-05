c34f28a414278eb8aa1fa6be081b23c5136ef835
jst0951 <jst0951@gmail.com>
2023-07-05 10:23:14 +0900

    [step-3 미구현 부분]게임 시작 및 종료 기능 구현(Main)

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -4,10 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 게임 시작 처리
+        waitUntilInputString("start");
+        // 게임 판 출력
+        Board board = new Board();
+        board.initialize();
+        System.out.println(board.print());
+        // 게임 종료 처리
+        waitUntilInputString("end");
+    }
+
+    public static void waitUntilInputString(String targetStr) {
         Scanner in = new Scanner(System.in);
+        final String errorMessage = "올바르지 않은 입력입니다. 게임을 시작하려면 start, 종료하려면 end를 눌러주세요.";
+
         while(true) {
-            String str = in.next(); // 입력 문자열 저장
-            System.out.println(str);
+            String str = in.next();
+            if(str.equals(targetStr)) {
+                break;
+            }
+            else {
+                System.out.println(errorMessage);
+            }
         }
     }
 }
