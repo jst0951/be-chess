09030c57e3cca26fa82fb9faad8602ba231f5064
jst0951 <jst0951@gmail.com>
2023-07-05 10:38:52 +0900

    [BoardTest]added target Test, now actual implementation on Board needed

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static utils.StringUtils.appendNewLine;
 
 public class BoardTest {
     private Board board;
@@ -40,5 +41,19 @@ public class BoardTest {
     public void print() throws Exception {
         board.initialize();
         System.out.println(board.print());
+    }
+
+    @Test
+    @DisplayName("초기화 후 최초에 보드가 올바르게 표시되어야 한다.")
+    public void create() throws Exception {
+        board.initialize();
+        assertThat(board.pieceCount());
+        String blankRank = appendNewLine("........");
+        assertThat(board.showBoard()).isEqualTo(
+                appendNewLine("RNBQKBNR") +
+                appendNewLine("PPPPPPPP") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("pppppppp") +
+                appendNewLine("rnbqkbnr"));
     }
 }
