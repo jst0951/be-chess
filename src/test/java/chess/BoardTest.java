3402fd2588a526e09bc978e1477d0808a8c9dc46
jst0951 <jst0951@gmail.com>
2023-07-05 11:06:41 +0900

    changed create() to new version, which tests initialization of pieces

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -17,20 +17,6 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("Board에 Piece 2개 생성 후 확인")
-    public void create() throws Exception {
-        Piece whitePawn = Piece.createWhitePawn();
-        board.add(whitePawn);
-        assertThat(board.size()).isEqualTo(1);
-        assertThat(board.findPawn(0)).isEqualTo(whitePawn);
-
-        Piece blackPawn = Piece.createBlackPawn();
-        board.add(blackPawn);
-        assertThat(board.size()).isEqualTo(2);
-        assertThat(board.findPawn(1)).isEqualTo(blackPawn);
-    }
-
-    @Test
     public void initialize() throws Exception {
         board.initialize();
         assertThat(board.getWhitePawnsResult()).isEqualTo("pppppppp");
