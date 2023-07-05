9604bc1d68eed6e3b5f96501121670d2117fd736
jst0951 <jst0951@gmail.com>
2023-07-05 17:20:48 +0900

    changed class variable types to enum, changed factory methods' input types

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -35,65 +35,65 @@ public class Piece {
     public static final String QUEEN_NAME = "queen";
     public static final String KING_NAME = "king";
 
-    private String color;
+    private Color color;
     private String name;
-    private char representation;
+    private Type type;
 
-    private Piece(String color, String name, char representation){
+    private Piece(Color color, String name, Type type){
         this.color = color;
         this.name = name;
-        this.representation = representation;
+        this.type = type;
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(WHITE_COLOR, PAWN_NAME, WHITE_PAWN_REPRESENTATION);
+        return new Piece(Color.WHITE, PAWN_NAME, Type.PAWN);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(BLACK_COLOR, PAWN_NAME, BLACK_PAWN_REPRESENTATION);
+        return new Piece(Color.BLACK, PAWN_NAME, Type.PAWN);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(WHITE_COLOR, KNIGHT_NAME, WHITE_KNIGHT_REPRESENTATION);
+        return new Piece(Color.WHITE, KNIGHT_NAME, Type.KNIGHT);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(BLACK_COLOR, KNIGHT_NAME, BLACK_KNIGHT_REPRESENTATION);
+        return new Piece(Color.BLACK, KNIGHT_NAME, Type.KNIGHT);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(WHITE_COLOR, ROOK_NAME, WHITE_ROOK_REPRESENTATION);
+        return new Piece(Color.WHITE, ROOK_NAME, Type.ROOK);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(BLACK_COLOR, ROOK_NAME, BLACK_ROOK_REPRESENTATION);
+        return new Piece(Color.BLACK, ROOK_NAME, Type.ROOK);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(WHITE_COLOR, BISHOP_NAME, WHITE_BISHOP_REPRESENTATION);
+        return new Piece(Color.WHITE, BISHOP_NAME, Type.BISHOP);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(BLACK_COLOR, BISHOP_NAME, BLACK_BISHOP_REPRESENTATION);
+        return new Piece(Color.BLACK, BISHOP_NAME, Type.BISHOP);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(WHITE_COLOR, QUEEN_NAME, WHITE_QUEEN_REPRESENTATION);
+        return new Piece(Color.WHITE, QUEEN_NAME, Type.QUEEN);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(BLACK_COLOR, QUEEN_NAME, BLACK_QUEEN_REPRESENTATION);
+        return new Piece(Color.BLACK, QUEEN_NAME, Type.QUEEN);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(WHITE_COLOR, KING_NAME, WHITE_KING_REPRESENTATION);
+        return new Piece(Color.WHITE, KING_NAME, Type.KING);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(BLACK_COLOR, KING_NAME, BLACK_KING_REPRESENTATION);
+        return new Piece(Color.BLACK, KING_NAME, Type.KING);
     }
 
-    public String getColor() {
+    public Color getColor() {
         return this.color;
     }
 
@@ -102,15 +102,20 @@ public class Piece {
     }
 
     public char getRepresentation() {
-        return this.representation;
+        if(this.color.equals(Color.WHITE)) {
+            return this.type.getWhiteRepresentation();
+        }
+        else {
+            return this.type.getBlackRepresentation();
+        }
     }
 
     // extra features
     public boolean isBlack() {
-        return this.color.equals(BLACK_COLOR);
+        return this.color.equals(Color.BLACK);
     }
 
     public boolean isWhite() {
-        return this.color.equals(WHITE_COLOR);
+        return this.color.equals(Color.WHITE);
     }
 }
