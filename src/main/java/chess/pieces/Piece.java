977f8d3753e916ca71a35bef4df9638cde64d4fb
jst0951 <jst0951@gmail.com>
2023-07-05 13:51:21 +0900

    changed color, type constants to Enum Type

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -1,8 +1,24 @@
 package chess.pieces;
 
 public class Piece {
-    public static final String WHITE_COLOR = "white";
-    public static final String BLACK_COLOR = "black";
+    public enum Color {
+        WHITE, BLACK, NOCOLOR;
+    }
+
+    public enum Type {
+        PAWN('p'),
+        ROOK('r'),
+        KNIGHT('n'),
+        BISHOP('b'),
+        QUEEN('q'),
+        KING('k'),
+        NO_PIECE('.');
+
+        private char representation;
+        Type(char representation) {
+            this.representation = representation;
+        }
+    }
 
     public static final String PAWN_NAME = "pawn";
     public static final String KNIGHT_NAME = "knight";
@@ -10,19 +26,6 @@ public class Piece {
     public static final String BISHOP_NAME = "bishop";
     public static final String QUEEN_NAME = "queen";
     public static final String KING_NAME = "king";
-
-    public static final char WHITE_PAWN_REPRESENTATION = 'p';
-    public static final char BLACK_PAWN_REPRESENTATION = 'P';
-    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
-    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
-    public static final char WHITE_ROOK_REPRESENTATION = 'r';
-    public static final char BLACK_ROOK_REPRESENTATION = 'R';
-    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
-    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
-    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
-    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
-    public static final char WHITE_KING_REPRESENTATION = 'k';
-    public static final char BLACK_KING_REPRESENTATION = 'K';
 
     private String color;
     private String name;
