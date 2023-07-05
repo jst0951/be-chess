efaaff235b3d582b3aa0790cfb649ce13cbacdc0
jst0951 <jst0951@gmail.com>
2023-07-05 17:46:40 +0900

    refactor : removed unused class variable 'name'

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -28,82 +28,68 @@ public class Piece {
         }
     }
 
-    public static final String BLANK_NAME = "blank";
-    public static final String PAWN_NAME = "pawn";
-    public static final String KNIGHT_NAME = "knight";
-    public static final String ROOK_NAME = "rook";
-    public static final String BISHOP_NAME = "bishop";
-    public static final String QUEEN_NAME = "queen";
-    public static final String KING_NAME = "king";
-
     private Color color;
-    private String name;
     private Type type;
 
-    private Piece(Color color, String name, Type type){
+    private Piece(Color color, Type type){
         this.color = color;
-        this.name = name;
         this.type = type;
     }
 
     public static Piece createBlank() {
-        return new Piece(Color.NOCOLOR, BLANK_NAME, Type.NO_PIECE);
+        return new Piece(Color.NOCOLOR, Type.NO_PIECE);
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(Color.WHITE, PAWN_NAME, Type.PAWN);
+        return new Piece(Color.WHITE, Type.PAWN);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(Color.BLACK, PAWN_NAME, Type.PAWN);
+        return new Piece(Color.BLACK, Type.PAWN);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(Color.WHITE, KNIGHT_NAME, Type.KNIGHT);
+        return new Piece(Color.WHITE, Type.KNIGHT);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(Color.BLACK, KNIGHT_NAME, Type.KNIGHT);
+        return new Piece(Color.BLACK, Type.KNIGHT);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(Color.WHITE, ROOK_NAME, Type.ROOK);
+        return new Piece(Color.WHITE, Type.ROOK);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(Color.BLACK, ROOK_NAME, Type.ROOK);
+        return new Piece(Color.BLACK, Type.ROOK);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(Color.WHITE, BISHOP_NAME, Type.BISHOP);
+        return new Piece(Color.WHITE, Type.BISHOP);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(Color.BLACK, BISHOP_NAME, Type.BISHOP);
+        return new Piece(Color.BLACK, Type.BISHOP);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(Color.WHITE, QUEEN_NAME, Type.QUEEN);
+        return new Piece(Color.WHITE, Type.QUEEN);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(Color.BLACK, QUEEN_NAME, Type.QUEEN);
+        return new Piece(Color.BLACK, Type.QUEEN);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(Color.WHITE, KING_NAME, Type.KING);
+        return new Piece(Color.WHITE, Type.KING);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(Color.BLACK, KING_NAME, Type.KING);
+        return new Piece(Color.BLACK, Type.KING);
     }
 
     public Color getColor() {
         return this.color;
-    }
-
-    public String getName() {
-        return this.name;
     }
 
     public Type getType() {
