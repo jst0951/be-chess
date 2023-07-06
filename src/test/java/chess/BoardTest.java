505e38735fbcdad1eadcfad1fa872657beb808de
jst0951 <jst0951@gmail.com>
2023-07-06 11:31:27 +0900

    added test for calculatePoint(), for case with pawns at same line

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -112,6 +112,33 @@ public class BoardTest {
 
         System.out.println(board.showBoard());
     }
+    @Test
+    @DisplayName("각자의 점수를 계산해야한다.(폰이 같은 세로줄에 있는 경우)")
+    public void calculatePointPawnProb() throws Exception {
+        board.initializeEmpty();
+
+        addPiece("b8", Piece.createBlackKing());
+        addPiece("c8", Piece.createBlackRook());
+        addPiece("a7", Piece.createBlackPawn());
+        addPiece("c7", Piece.createBlackPawn());
+        addPiece("d7", Piece.createBlackBishop());
+        addPiece("b6", Piece.createBlackPawn());
+        addPiece("e6", Piece.createBlackQueen());
+
+        addPiece("f4", Piece.createWhiteKnight());
+        addPiece("g4", Piece.createWhiteQueen());
+        addPiece("f3", Piece.createWhitePawn());
+        addPiece("h3", Piece.createWhitePawn());
+        addPiece("f2", Piece.createWhitePawn());
+        addPiece("g2", Piece.createWhitePawn());
+        addPiece("e1", Piece.createWhiteRook());
+        addPiece("f1", Piece.createWhiteKing());
+
+        assertThat(board.calculatePoint(Color.BLACK)).isEqualTo(20, withPrecision(0.01));
+        assertThat(board.calculatePoint(Color.WHITE)).isEqualTo(19.5, withPrecision(0.01));
+
+        System.out.println(board.showBoard());
+    }
     private void addPiece(String position, Piece piece) {
         board.move(position, piece);
     }
