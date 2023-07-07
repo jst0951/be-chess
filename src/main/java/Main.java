d2247d291cf523c0d251bbdf496cdc9703f2f0b3
jst0951 <jst0951@gmail.com>
2023-07-07 11:18:18 +0900

    added move operation in Main, refactored some code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,60 @@
 import chess.Board;
+import chess.Position;
+
+import static utils.StringUtils.appendNewLine;
 
 import java.util.Scanner;
 
 public class Main {
+    private static final String ERROR_MESSAGE_START = "올바르지 않은 입력입니다. 게임을 시작하려면 start를 눌러주세요.";
+    private static final String INFO_GAME_START = "게임이 시작되었습니다. 말을 움직여주세요.";
+    private static final String ERROR_MESSAGE_MOVE = "올바르지 않은 입력입니다. 'move b2 b3'과 같은 형태를 입력해주세요.";
+    private static final String ERROR_MESSAGE_END = "게임을 종료하시려면 end를 눌러주세요.";
+    private static final String ERROR_MESSAGE_GAME = appendNewLine(ERROR_MESSAGE_MOVE) + ERROR_MESSAGE_END;
+    private static final String INFO_GAME_END = "게임이 종료되었습니다.";
+
     public static void main(String[] args) {
         // 게임 시작 처리
         waitUntilInputString("start");
-        // 게임 판 출력
+        // 게임 진행
+        playGame();
+    }
+
+    private static void playGame() {
         Board board = new Board();
         board.initialize();
         System.out.println(board.showBoard());
-        // 게임 종료 처리
-        waitUntilInputString("end");
-    }
 
-    public static void waitUntilInputString(String targetStr) {
         Scanner in = new Scanner(System.in);
-        final String errorMessage = "올바르지 않은 입력입니다. 게임을 시작하려면 start, 종료하려면 end를 눌러주세요.";
 
         while(true) {
-            String str = in.next();
-            if(str.equals(targetStr)) {
+            String inputString = in.nextLine();
+            if(inputString.equals("end")) {
+                System.out.println(INFO_GAME_END);
+                break;
+            }
+            else if(inputString.startsWith("move")) {
+                String[] tokens = inputString.split(" ");
+                board.move(new Position(tokens[1]), new Position(tokens[2]));
+                System.out.println(board.showBoard());
+            }
+            else {
+                System.out.println(ERROR_MESSAGE_GAME);
+            }
+        }
+    }
+
+    private static void waitUntilInputString(String targetStr) {
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            String inputString = in.next();
+            if(inputString.equals(targetStr)) {
+                System.out.println(INFO_GAME_START);
                 break;
             }
             else {
-                System.out.println(errorMessage);
+                System.out.println(ERROR_MESSAGE_START);
             }
         }
     }
